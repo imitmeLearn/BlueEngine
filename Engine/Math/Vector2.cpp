16ac3c5e46ce8b27f0ce63ff865044237c145860
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-02-11 18:10:47 +0900

    07 Math - 0 기본

---
+++ Engine/Math/Vector2.cpp
@@ -0,0 +1,40 @@
+﻿#include <Windows.h>
+#include <cmath>
+#include "Vector2.h"
+
+namespace Blue
+{
+const Vector2 Vector2::Zero = Vector2(0.f,0.f);
+const Vector2 Vector2::One = Vector2(1.f,1.f);
+const Vector2 Vector2::Right = Vector2(1.f,0.f);
+const Vector2 Vector2::Up = Vector2(0.f,1.f);
+
+std::wstring Vector2::ToString()
+{
+	wchar_t buffer[256];
+	swprintf_s(buffer,256,TEXT("(%f,%f)"),x,y);
+
+	return std::wstring();
+}
+float Vector2::Length()
+{
+	return sqrtf(x*x +y*y);
+}
+float Vector2::Dot()
+{
+	return 0.0f;
+}
+float Vector2::Dot(const Vector2& other)
+{
+	return (x*other.x)+ (y*other.y);
+}
+Vector2 Vector2::Normalized()
+{
+	float length = Length();
+	return Vector2(x/length,y/length);
+}
+bool Vector2::Equals(const Vector2 & other)
+{
+	return x == other.x && y==other.y;	//float 오차발생할 수 있어서, 그 차가 특정 값 이하면, 절대 값 취해서, 오차범위 이내면 같다고 표현하다. 적당히 맞으니,엄밀히 어차범위 따져서, 작업을 해줘야 한다. 보통 구해서 쓰고, float 표준라이브러리에 있을 수 있는데, 부동 소수 어떻게 저장되는지,
+}
+}
---
+++ Engine/Math/Vector2.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <string>
+namespace Blue
+{
+class Vector2
+{
+public:
+	union
+	{
+		struct
+		{
+			float x ;
+			float y ;
+		};
+		float data[2];
+	};
+
+	Vector2()
+		: x(x),y(y)
+	{}
+
+	Vector2(float value)
+		: x(value),y(value)
+	{}
+	Vector2(float x,float y)
+		: x(x),y(y)
+	{}
+
+	//기본값.//전역변수이기에. 클래스 선언될 떄, 초기화 되어서, 바로 읽어서, 쓸 쑤 있다.
+	static const Vector2 Zero;
+	static const Vector2 One;
+	static const Vector2 Right;
+	static const Vector2 Up;
+
+	////(x, y)문자열로 제공해 만들어주는.... 문자열 생성 함수.
+	std::wstring ToString();
+
+	//길이 구하기
+	float Length();
+	//float LengthSquared();
+	float Dot();
+	float Dot(const Vector2& other);
+	Vector2 Normalized();
+	bool Equals(const Vector2& other);
+};
+}
