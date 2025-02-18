884423066022129d3e72f22cbdf80b4334def751
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-02-18 09:07:55 +0900

    Mesh. 0.1 임시 리소스 생성 분리해 오기

--- Engine/Render/Mesh.cpp
+++ Engine/Render/Mesh.cpp
@@ -24,6 +24,42 @@ MeshData::MeshData(
 	this->vertices.assign(vertices.begin(),vertices.end());
 	stride = Vertex::Stride();
 	this->indices.assign(indices.begin(),indices.end());
+
+	//@Temp 임시 리소스 생성 - 버텍스
+	D3D11_BUFFER_DESC vertexBufferDesc = {};
+	vertexBufferDesc.ByteWidth = Vector3::Stride() *3;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER; //필수
+
+	D3D11_SUBRESOURCE_DATA vertexData = {};	//정점 데이터 //여러데이터, 하나로 넣을 떄 사용
+	vertexData.pSysMem =	vertices;
+
+	result= device -> CreateBuffer(&vertexBufferDesc,&vertexData,&vertexBuffer);		//버퍼(Buffer) 메모리 덩어리 - 버텍스
+	if(FAILED(result))	//결과 확인
+	{
+		MessageBoxA(nullptr,"fail to CreateBuffer - vertex  ","ERROR",MB_OK);
+		__debugbreak();
+	}
+
+	//인덱스 (색인) 버퍼 생성 (정점을 조립하는 순서)
+	int indeice[] =
+	{
+		0,1,2
+	};
+
+	//@Temp 임시 리소스 생성 - 인덱스
+	D3D11_BUFFER_DESC indexBufferDesc = {};
+	indexBufferDesc.ByteWidth = sizeof(int)  *3;
+	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER; //필수
+
+	D3D11_SUBRESOURCE_DATA indexData = {};	//정점 데이터 //여러데이터, 하나로 넣을 떄 사용
+	indexData.pSysMem =	indeice;
+
+	result=	device -> CreateBuffer(&indexBufferDesc,&indexData,&indexBuffer);	//버퍼(Buffer) 메모리 덩어리  - 인덱스
+	if(FAILED(result))	//결과 확인
+	{
+		MessageBoxA(nullptr,"fail to CreateBuffer -index ","ERROR",MB_OK);
+		__debugbreak();
+	}
 }
 void MeshData::Bind()
 {}
