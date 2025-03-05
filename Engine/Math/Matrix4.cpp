e023550151aea0ebd0cfd6c94e566f591b963614
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-03-05 15:33:42 +0900

    행렬구현-회전

---
+++ Engine/Math/Matrix4.cpp
@@ -0,0 +1,122 @@
+﻿#include "Matrix4.h"
+#include <cmath>
+
+#include "DirectXMath.h"
+namespace Blue
+{
+Matrix4 Matrix4::Identity = Matrix4();
+Matrix4::Matrix4()
+{
+	memset(elements,0,sizeof(float)*16);	// 원소elements 접근은, 변수가 편함.
+	m00=m11=m22=m33=1.f;
+
+	//XMmatrixrotation;	//라디안으로 변환해야 함.
+}
+Matrix4::Matrix4(const Matrix4 & other)
+{
+	memcpy(elements,other.elements,sizeof(float)*16);
+}
+Matrix4 Matrix4::Rotation(const Vector3 & rotation)
+{
+	return Rotation(rotation.x,rotation.y,rotation.z);
+}
+Matrix4 Matrix4::Rotation(float x,float y,float z)	//z 가 0이면, 짐벌락 안남.
+{
+	return RotationX(x) * RotationY(y) * RotationZ(z);
+}
+Matrix4 Matrix4::RotationX(float angle)
+{
+	Matrix4 m;	//반환을 위한 행렬 변수 선언(단위 행렬).
+
+	float cosAngle = std::cos(angle*degreeToRadian);		//행렬 계산을 위한 삼각함수 값 저장.
+	float sinAngle = std::sin(angle*degreeToRadian);		//행렬 계산을 위한 삼각함수 값 저장.
+
+	//행렬
+	m.m00 = 1.f;			m.m01 = 0.f;			m.m02 = 0.f;			m.m03 = 0.f;
+	m.m10 = 0.f;			m.m11 = cosAngle;		m.m12 = sinAngle;		m.m13 = 0.f;
+	m.m20 = 0.f;			m.m21 = -sinAngle;		m.m22 = cosAngle;		m.m23 = 0.f;
+	m.m30 = 0.f;			m.m31 = 0.f;			m.m32 = 0.f;			m.m33 = 1.f;
+
+	return Matrix4();
+}
+
+Matrix4 Matrix4::RotationY(float angle)	//두번째 요소를 안쓴다.
+{
+	Matrix4 m;	//반환을 위한 행렬 변수 선언(단위 행렬).
+
+	float cosAngle = std::cos(angle*degreeToRadian);		//행렬 계산을 위한 삼각함수 값 저장.
+	float sinAngle = std::sin(angle*degreeToRadian);		//행렬 계산을 위한 삼각함수 값 저장.
+
+	//행렬
+	m.m00 = cosAngle;		m.m01 = 0.f;			m.m02 = -sinAngle;		m.m03 = 0.f;
+	m.m10 = 0.f;			m.m11 = 1.f;			m.m12 = 0.f;			m.m13 = 0.f;
+	m.m20 = sinAngle;		m.m21 = 0.f;			m.m22 = cosAngle;		m.m23 = 0.f;
+	m.m30 = 0.f;			m.m31 = 0.f;			m.m32 = 0.f;			m.m33 = 1.f;
+
+	return Matrix4();
+}
+Matrix4 Matrix4::RotationZ(float angle)		//세번째 요소 안쓴다.
+{
+	Matrix4 m;	//반환을 위한 행렬 변수 선언(단위 행렬).
+
+	float cosAngle = std::cos(angle*degreeToRadian);		//행렬 계산을 위한 삼각함수 값 저장.
+	float sinAngle = std::sin(angle*degreeToRadian);		//행렬 계산을 위한 삼각함수 값 저장.
+
+	//행렬
+	m.m00 = cosAngle;		m.m01 = sinAngle;		m.m02 = 0.f;			m.m03 = 0.f;
+	m.m10 = -sinAngle;		m.m11 = cosAngle;		m.m12 = 0.f;			m.m13 = 0.f;
+	m.m20 = 0.f;			m.m21 = 0.f;			m.m22 = 1.f;			m.m23 = 0.f;
+	m.m30 = 0.f;			m.m31 = 0.f;			m.m32 = 0.f;			m.m33 = 1.f;
+
+	return Matrix4();
+}
+Matrix4& Matrix4::operator=(const Matrix4& other)
+{
+	//Matrix4 m;	//반환을 위한 행렬 변수 선언(단위 행렬).
+	memcpy(elements,other.elements,sizeof(float)*16);
+	return *this;
+}
+Matrix4& Matrix4::operator*(const Matrix4& other)
+{
+	Matrix4 m;
+
+	m.m00 =		m00 * other.m00 		+m01 *other.m10			+m02 *other.m20		+m03 *other.m30;
+	m.m01 =		m00 * other.m01 		+m01 *other.m11			+m02 *other.m21		+m03 *other.m31;
+	m.m02 =		m00 * other.m02 		+m01 *other.m12			+m02 *other.m22		+m03 *other.m32;
+	m.m03 =		m00 * other.m03 		+m01 *other.m13			+m02 *other.m23		+m03 *other.m33;
+
+	m.m10 =		m10 * other.m00 		+m11 *other.m10			+m12 *other.m20		+m13 *other.m30;
+	m.m11 =		m10 * other.m01 		+m11 *other.m11			+m12 *other.m21		+m13 *other.m31;
+	m.m12 =		m10 * other.m02 		+m11 *other.m12			+m12 *other.m22		+m13 *other.m32;
+	m.m13 =		m10 * other.m03 		+m11 *other.m13			+m12 *other.m23		+m13 *other.m33;
+
+	m.m20 =		m20 * other.m00 		+m21 *other.m10			+m22 *other.m20		+m23 *other.m30;
+	m.m21 =		m20 * other.m01 		+m21 *other.m11			+m22 *other.m21		+m23 *other.m31;
+	m.m22 =		m20 * other.m02 		+m21 *other.m12			+m22 *other.m22		+m23 *other.m32;
+	m.m23 =		m20 * other.m03 		+m21 *other.m13			+m22 *other.m23		+m23 *other.m33;
+
+	m.m30 =		m30 * other.m00 		+m31 *other.m10			+m32 *other.m20		+m33 *other.m30;
+	m.m31 =		m30 * other.m01 		+m31 *other.m11			+m32 *other.m21		+m33 *other.m31;
+	m.m32 =		m30 * other.m02 		+m31 *other.m12			+m32 *other.m22		+m33 *other.m32;
+	m.m33 =		m30 * other.m03 		+m31 *other.m13			+m32 *other.m23		+m33 *other.m33;
+
+	return m;
+}
+Matrix4 & Matrix4::operator*=(const Matrix4& other)
+{
+	*this = *this * other;
+	return *this;
+}
+Vector3 operator*(const Vector3 & vector,const Matrix4 & matrix)
+{
+	Vector3 result;
+	result.x = vector.x * matrix.m00		+ vector.y * matrix.m10			+ vector.z * matrix.m20;
+	result.y = vector.x * matrix.m01		+ vector.y * matrix.m11			+ vector.z * matrix.m21;
+	result.z = vector.x * matrix.m02		+ vector.y * matrix.m12			+ vector.z * matrix.m22;
+	return result;
+}
+Vector3 operator*(const Matrix4 & matrix,const Vector3 & vector)
+{
+	return vector * matrix;
+}
+}
---
+++ Engine/Math/Matrix4.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+#include "Core\Type.h"
+#include "Vector3.h"
+namespace Blue
+{
+class Matrix4
+{
+public:
+	Matrix4();
+	Matrix4(const Matrix4& other);//복사생성자
+	~Matrix4() = default;
+
+	//메모리 너비(단위:바이트)
+	static uint32 Stride()
+	{
+		return sizeof(Matrix4);
+	}
+	static Matrix4 Rotation(const Vector3& rotation);
+	static Matrix4 Rotation(float x,float y,float z);
+	static Matrix4 RotationX(float angle);
+	static Matrix4 RotationY(float angle);
+	static Matrix4 RotationZ(float angle);
+
+	//operator overloading
+	Matrix4& operator= (const Matrix4& other);
+	Matrix4& operator*(const Matrix4& other);
+	Matrix4& operator*= (const Matrix4& other);
+
+	friend Vector3 operator* (const Vector3& vector,const Matrix4& matrix);
+	friend Vector3 operator* (const Matrix4& matrix,const Vector3& vector);
+
+	static Matrix4 Identity;
+	//Degree <->Radian  constexpr를 쓰면, 부를때마다, 컴파일시점에 계산된 결과만 저장해 둔다. !!!!!
+	static constexpr float degreeToRadian = 3.141592f / 180.f;
+	static constexpr float radiantoDegree = 180.f / 3.141592f ;
+
+private:
+	union //공용체 : 메모리를 공유한다. //덩치큰것을 기준으로 만들고, 둘이 같은거!	//메모리구조는 1자로 잡힘.
+	{
+		struct
+		{
+			float m00,m01,m02,m03;
+			float m10,m11,m12,m13;
+			float m20,m21,m22,m23;
+			float m30,m31,m32,m33;
+		};
+		float elements[4*4];
+	};
+};
+}
