ee7e5fa13e4d3a2a7fec6279d832fe2f76a08d06
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-02 11:06:45 +0900

    (완) 액터 만들기

---
+++ Engine/Actor/Actor.cpp
@@ -0,0 +1,30 @@
+﻿#include "Actor.h"
+namespace Blue
+{
+Actor::Actor()
+{}
+Actor::~Actor()
+{}
+void Actor::BeginPlay()
+{
+	hasInitialized = true;
+}
+void Actor::Tick(float deltaTime)
+{}
+void Actor::Draw()
+{
+	transform.Bind();	//트랜스폼 바인드 (셰이더에 데이터 전달)
+}
+void Actor::Destroy()
+{
+	hasDestroyed = true;
+}
+const bool Actor::IsActive() const
+{
+	return isActive && !hasDestroyed;
+}
+const bool Actor::HasInitialized() const
+{
+	return hasInitialized;
+}
+}
---
+++ Engine/Actor/Actor.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <string>
+#include <Math\Transform.h> //설계 시 고려할 사항.
+
+namespace Blue
+{
+class Actor
+{
+public:
+	Actor();
+	virtual ~Actor(); //기반클래스 여서, virtual
+	virtual void BeginPlay();
+	virtual void Tick(float deltaTime);
+	virtual void Draw();
+	void Destroy();
+	const bool IsActive() const;
+	const bool HasInitialized() const;
+public:
+	Transform transform; //스택변수 //엑터의 TRS 관리하는 트랜스폼. 변환정보
+private:
+protected:
+	std::wstring name = TEXT("Actor");	//액터 이름. 일반적으로 Hash 로 변환해서 사용함.
+	bool hasInitialized = false; //액터 초기화 여부.
+	bool isActive = false;		//활성화와 여부
+	bool hasDestroyed = false;	//삭제 여부
+};
+}
