5f56f12b4fc13027ee680c96f949dd71f6a607b0
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-16 12:24:51 +0900

    0416-06- 쉐이더 공통 요소 묶음 hlsli

--- Engine/HLSLShader/TextureMappingPixelShader.hlsl
+++ Engine/HLSLShader/TextureMappingPixelShader.hlsl
@@ -1,3 +1,5 @@
+#include "Common.hlsli"
+
 struct PixelInput
 {
 	float4 position : SV_POSITION;  // float3 position : POSITION;	 //화면엔 그려지나, 매프레임, 콘솔 오류남!  //https://learn.microsoft.com/ko-kr/windows/win32/direct3dhlsl/dx-graphics-hlsl-semantics //
@@ -21,24 +23,12 @@ float4 main(PixelInput input) : SV_TARGET
 	//World Normal
 	float3 worldNormal = normalize(input.normal);
 
-///선택
-	//Dot. Lambert
-	float nDotl =saturate (dot(worldNormal, -lightDir));
-	//Half Lambert.
-	float maxLight =0.8f;
-	nDotl = (nDotl * maxLight) + (1 - maxLight);	// 밝아짐 기본 0.5 / 0.8인경우,  0.2 더해줘야 해 그래서, 공식화 한다면,
-	nDotl = pow(nDotl, 2);	//밝음 조절 가능.
-	
+	//Lambert
+	float nDotl =CalcLambert(worldNormal, lightDir);
 
 	//final
 	float4 finalColor = texColor * nDotl;
 
 
-	//texColor *=float4(input.color, 1.0f); 
-	//return texColor;
-
-	//return float4(input.color, 1.0f);
-	//return float4(input.texCoord, 0.0f,1.0f);
-	//return float4(lightIntensity, lightIntensity, lightIntensity, 1);
 	return finalColor;
 }
