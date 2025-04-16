13d2c138b414ba55c1fb2ece0f4437579465f53c
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-16 09:45:45 +0900

    0416-01-SphereMesh 모델링 로드

---
+++ Engine/Render/SphereMesh.cpp
@@ -0,0 +1,15 @@
+﻿#include "SphereMesh.h"
+#include "Resource\ModelLoader.h"
+
+namespace Blue
+{
+SphereMesh::SphereMesh()
+{
+	// 모델로더
+	std::weak_ptr<MeshData> mesh;
+	if(ModelLoader::Get().Load("Sphere.obj",mesh))
+	{
+		meshes.emplace_back(mesh);
+	}
+}
+}
---
+++ Engine/Render/SphereMesh.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "Mesh.h"
+
+namespace Blue
+{
+class SphereMesh: public Mesh  //삼각형 메쉬 클래스
+{
+public:
+	SphereMesh();
+
+private:
+};
+}
