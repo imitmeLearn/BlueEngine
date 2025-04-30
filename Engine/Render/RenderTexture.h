ac59df636863767b15b51386c752f51a006eda90
imitme_unrealPU <37390101+imitme@users.noreply.github.com>
2025-04-30 14:45:47 +0900

    0430 DS 생성

--- Engine/Render/RenderTexture.h
+++ Engine/Render/RenderTexture.h
@@ -10,7 +10,7 @@ class RenderTexture: public Texture
 public:
 	RenderTexture(uint32 width = 0,uint32 height = 0);
 	virtual ~RenderTexture();
-
+	//GETTER - RendertargetView
 	ID3D11RenderTargetView* GetRenderTarget() const
 	{
 		return renderTargetView;
@@ -21,6 +21,11 @@ public:
 		return &renderTargetView;
 	}
 
+	//GETTER - Depth stencil view
+	ID3D11DepthStencilView* DepthStencilView()
+	{
+		return depthStencilView;
+	}
 private:
 	ID3D11RenderTargetView* renderTargetView = nullptr;
 	ID3D11DepthStencilView* depthStencilView = nullptr;
